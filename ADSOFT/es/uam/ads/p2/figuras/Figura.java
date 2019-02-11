45f7784517cdfdf6ebd24dcadd4a95661ca6d5de
masternico97 <36814913+masternico97@users.noreply.github.com>
2019-02-11 23:41:28 +0100

    Figura comentada
    No he compilado ni generado documentación

--- ADSOFT/es/uam/ads/p2/figuras/Figura.java
+++ ADSOFT/es/uam/ads/p2/figuras/Figura.java
@@ -7,23 +7,32 @@ package es.uam.ads.p2.figuras;
  * Grupo de prácticas: 2213
  */
 
- public abstract class Figura {
+public abstract class Figura {
 
     public String toString() {
         return getClass().getSimpleName() + " [area=" + getArea() + ", perimetro=" + getPerimetro() + "]";
     }
 
+    /**
+     * Devuelve el perímetro de la figura
+     * @return perímetro de la figura
+     */
     abstract double getPerimetro();
 
+    /**
+     * Devuelve el área de la figura
+     * @return área de la figura
+     */
     abstract double getArea();
 
     /**
-    *
-    *
+    * Devuelve si nuestra figura es mayor
+    * a la figura que recibe la funcion.
+    * @param figura figura a comparar
     * @return boolean que dice si es mayor o no.
     */
     public boolean esMayor(Figura figura) {
         return getArea() > figura.getArea();
     }
 
- }
+}
