4ec276c246e5bfa7e5fadb04dce0ea0b6cbc41a5
masternico97 <36814913+masternico97@users.noreply.github.com>
2019-03-19 14:14:29 +0100

    Add files via upload

--- ADSOFT/GR2213_P4_AlvaroNicolas/src/es/uam/eps/ads/p4/RecomendadorVecinos.java
+++ ADSOFT/GR2213_P4_AlvaroNicolas/src/es/uam/eps/ads/p4/RecomendadorVecinos.java
@@ -3,8 +3,33 @@ package es.uam.eps.ads.p3;
 import java.util.*;
 
 public class RecomendadorVecinos implements Recomendador {
-    Long usuario;
-    List<Tupla> recomendaciones = new ;
+    private ModeloDatos datos;
+    private Long usuario;
+    private List<Tupla> recomendaciones = new ArrayList<>();
 
-    public Recomendacion recomienda(Long u, int longitudRecomendacion) throws RecomendacionInvalida;
+
+    public RecomendadorVecinos(ModeloDatos datos, Long usuario){
+        this.datos = datos;
+        this.usuario = usuario;
+    }
+
+    public Recomendacion recomienda(Long u, int longitudRecomendacion){
+        int i = 0;
+        Set<Long> userItems;
+
+        userItems = itemsInUsuario();
+        while(i < longitudRecomendacion){
+            for(Long currentItem: datos.getItemsUnicos()){
+                if(!currentItem.contains(currentItem)){
+                    
+                }
+            }
+        }
+    }
+
+    /** Devuelve los items que tiene un usuario
+    */
+    private Set<Long> itemsInUsuario (Long u){
+        return datos.getPreferenciasUsuario(u).keySet();
+    }
 }
