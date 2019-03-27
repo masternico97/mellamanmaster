a428b3905e4ed6bb3e43684d405de4eb1b735508
masternico97 <36814913+masternico97@users.noreply.github.com>
2019-03-27 10:49:56 +0100

    Add files via upload

--- MICRO/pra3/pract3.c
+++ MICRO/pra3/pract3.c
@@ -16,9 +16,9 @@
 
 /* Ejercicio 1 */
 
-unsigned char computeControlDigit(unsigned char* barCodeDigits);
-void createBarCode(int countryCode, long int companyCode, long int productCode, unsigned char controlDigit, unsigned char* out_barCodeASCII);
-void decodeBarCode(unsigned char* in_barCodeASCII, unsigned int* countryCode, unsigned long int* companyCode, unsigned long int* productCode, unsigned char* controlDigit);
+unsigned char computeControlDigit(char* barCodeASCII);
+//void createBarCode(int countryCode, unsigned int companyCode, unsigned long productCode, unsigned char controlDigit, unsigned char* out_barCodeASCII);
+//void decodeBarCode(unsigned char* in_barCodeASCII, unsigned int* countryCode, unsigned int* companyCode, unsigned long* productCode, unsigned char* controlDigit);
 
 
 
@@ -28,31 +28,33 @@ void decodeBarCode(unsigned char* in_barCodeASCII, unsigned int* countryCode, un
 int main( void ){
 	char barCodeStr[14],barCodeStrCorregido[14];
 	unsigned char barCodeDigits[13];
-	unsigned int  controlDigitCheck, countryCode;
-	unsigned long int companyCode, productCode;
-	unsigned char controlDigit;
+	unsigned int  countryCode, companyCode;
+	unsigned long productCode;
+	unsigned char controlDigitCheck, controlDigit;
 
 	printf("Introduzca nuevo codigo de barras de 13 digitos: ");
 	scanf("%s", &barCodeStr);
 
 		
-	decodeBarCode(barCodeStr, &countryCode, &companyCode, &productCode, &controlDigit);
-	printf("Codigo de barras leido:\n");
-	printf("- Codigo de Pais - %u -\n",countryCode);
-	printf("- Codigo de Empresa - %lu -\n",companyCode);
-	printf("- Codigo de Producto - %lu -\n",productCode);
-	printf("- Codigo de Control - %u -\n",controlDigit);
+	//decodeBarCode(barCodeStr, &countryCode, &companyCode, &productCode, &controlDigit);
+	//printf("Codigo de barras leido:\n");
+	//printf("- Codigo de Pais - %u -\n",countryCode);
+	//printf("- Codigo de Empresa - %u -\n",companyCode);
+	//printf("- Codigo de Producto - %lu -\n",productCode);
+	//printf("- Codigo de Control - %u -\n",controlDigit);
 
-	ControlDigitCheck = computeControlDigit(barCodeDigits);
+	controlDigitCheck = computeControlDigit(barCodeStr);
+	printf("%c", controlDigitCheck);
 	
-	if(controlDigit != controlDigitCheck) {
-		printf("Error en codigo de control. Leido %u vs Calculado %u\n", controlDigit, controlDigitCheck);
-		printf("Corrigiendo codigo de barras...\n");
-		createBarCode(countryCode,companyCode,productCode,controlDigitCheck,barCodeStrCorregido);
-		printf("Codigo de barras corregido es: %s\n",bardCodeStrCorregido);
-	} else {
-		printf("Codigo de control %u es correcto para el codigo de barras %s\n", controlDigit, barCodeStr);
-	}
+	//if(controlDigit != controlDigitCheck){
+	//	printf("Error en codigo de control. Leido %u vs Calculado %u\n", controlDigit, controlDigitCheck);
+	//	printf("Corrigiendo codigo de barras...\n");
+	//	createBarCode(countryCode,companyCode,productCode,controlDigitCheck,barCodeStrCorregido);
+	//	printf("Codigo de barras corregido es: %s\n",barCodeStrCorregido);
+	//}
+	//else{
+	//	printf("Codigo de control %u es correcto para el codigo de barras %s\n", controlDigit, barCodeStr);
+	//}
 	
 	
 	return 0;
