6e481501d3b7ab40b55216c0c0c389f010b398d4
masternico97 <36814913+masternico97@users.noreply.github.com>
2019-04-01 10:59:01 +0200

    Add files via upload

---
+++ ADSOFT/GR2213_P4_AlvaroNicolas/src/es/uam/eps/ads/p4/Apartado5.java
@@ -0,0 +1,18 @@
+package es.uam.eps.ads.p4;
+
+/**
+ * Apartado 5 de la practica que comprueba que todo funciona correctamente
+ * @author <a href="mailto:nicolas.serranos@estudiante.uam.es">Nicolas Serrano</a>
+ * @author <a href="mailto:alvaro.sanchezromero@estudiante.uam.es">Alvaro Sanchez</a>
+ * Grupo de practicas: 2213
+ */
+
+public class Apartado5{
+    public static void main(String[] args) {
+        if(args.length != 2){
+            return;
+        }
+
+    }
+
+}
