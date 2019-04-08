7f861e4373c21bdfab0f1524c105e2e82a748188
masternico97 <36814913+masternico97@users.noreply.github.com>
2019-04-08 08:51:21 +0200

    Delete Metrica.java

--- ADSOFT/adsoftp4/src/es/uam/eps/ads/p4/Metrica.java
+++
@@ -1,8 +0,0 @@
-package es.uam.eps.ads.p4;
-
-import java.util.*;
-
-public interface Metrica {
-    public double evalua(Recomendacion rec, int n) throws UsuarioNoRelevante;
-    public Set<Long> getItemsRelevantes(Long u);
-}
