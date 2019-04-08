239b95adcabcf2644e1c8c515d355ae4f2e588ee
masternico97 <36814913+masternico97@users.noreply.github.com>
2019-04-08 08:55:12 +0200

    Delete Recomendador.java

--- ADSOFT/adsoftp4/src/es/uam/eps/ads/p4/Recomendador.java
+++
@@ -1,6 +0,0 @@
-package es.uam.eps.ads.p4;
-
-public interface Recomendador {
-    public Recomendacion recomienda(Long u, int longitudRecomendacion) throws RecomendacionInvalida;
-    //Otros metodos que loslas estudiantes crean oportuno
-}
