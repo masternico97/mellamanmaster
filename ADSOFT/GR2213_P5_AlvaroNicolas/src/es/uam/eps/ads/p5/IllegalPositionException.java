698abb1b4f133ba6cf3ae615a7b53eacede90075
mellamansanchez <38490771+mellamansanchez@users.noreply.github.com>
2019-04-08 11:15:59 +0200

    Add files via upload

---
+++ ADSOFT/GR2213_P5_AlvaroNicolas/src/es/uam/eps/ads/p5/IllegalPositionException.java
@@ -0,0 +1,10 @@
+package es.uam.eps.ads.p5;
+
+public class IllegalPositionException extends Exception {
+    public IllegalPositionException(int i, int j) {
+        System.out.println("Indice fuera de la posicion maxima (i = "+i+", j = "+j+")");
+    }
+    public IllegalPositionException() {
+        System.out.println("Indice fuera de la posicion maxima");
+    }
+}
