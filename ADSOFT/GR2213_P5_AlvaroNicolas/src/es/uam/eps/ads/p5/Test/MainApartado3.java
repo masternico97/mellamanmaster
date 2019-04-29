0a3c3b709a8ed2ad9e300d41516977358bafc49a
masternico97 <36814913+masternico97@users.noreply.github.com>
2019-04-29 10:54:13 +0200

    Add files via upload

---
+++ ADSOFT/GR2213_P5_AlvaroNicolas/src/es/uam/eps/ads/p5/Test/MainApartado3.java
@@ -0,0 +1,18 @@
+package es.uam.eps.ads.p5.Test;
+
+import es.uam.eps.ads.p5.Agent.BasicAgent;
+import es.uam.eps.ads.p5.Agent.BasicSimulator;
+
+public class MainApartado3 {
+
+    public static void main(String args[]) {
+    	BasicSimulator s = new BasicSimulator(10,10);
+    	BasicAgent random = new BasicAgent("random");
+    	BasicAgent outer = new BasicAgent("outer");
+    	
+    	s.create(random, 10, 5, 5);
+        s.create(outer, 10, 7, 7);
+        s.run(2);
+
+    }
+}
