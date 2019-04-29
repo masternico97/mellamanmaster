7fdbf07bc98c7f72fdaaf9ed7b1bdffb31126b0b
mellamansanchez <38490771+mellamansanchez@users.noreply.github.com>
2019-04-29 10:48:27 +0200

    Add files via upload

---
+++ ADSOFT/GR2213_P5_AlvaroNicolas/src/es/uam/eps/ads/p5/Test/MainApartado4.java
@@ -0,0 +1,42 @@
+package es.uam.eps.ads.p5.Test;
+
+import es.uam.eps.ads.p5.Agent.Agent;
+import es.uam.eps.ads.p5.Agent.Cell;
+import es.uam.eps.ads.p5.Agent.Simulator;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class MainApartado4 {
+    public static void main(String [] args) {
+        Simulator s = new Simulator(10,10);
+        Agent random = new Agent("random");
+        Agent outer = new Agent("outer");
+        random.addBehaviour( agent -> { // se ejecuta siempre, movemos el agente a una casilla aleatoria vecina
+            List<Cell> neighbours = agent.cell().neighbours();
+            Cell destination = neighbours.get(new Random().nextInt(neighbours.size()));
+            agent.moveTo(destination);
+            return true;
+        });
+        outer.addBehaviour( agent -> agent.cell().getElement().size()>5, // Lo ejecutamos si hay más de 5 agentes
+                // en la celda actual
+                agent -> { // nos movemos a la celda destino con menos agentes
+                    List<Cell> neighbours = agent.cell().neighbours();
+                    Integer minAgents = neighbours.stream().
+                            mapToInt( c -> c.agents().size() ).
+                            min( ).
+                            getAsInt();
+                    List<Cell> destinations = neighbours.stream().
+                            filter( c -> c.agents().size() == minAgents ).
+                            collect(Collectors.toList());
+                    Cell destination = destinations.get(new Random().nextInt(destinations.size()));
+                    agent.moveTo(destination);
+                    return true;
+                });
+        s.create(random, 100, 5, 5); // Crear 100 agentes random
+        s.create(outer, 100, 7, 7); // Crear 100 agentes "outer"
+        s.run(60); // Ejecutar 60 pasos de simulacion
+
+    }
+}
